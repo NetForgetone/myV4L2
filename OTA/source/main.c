675cb6844f98f1d8efa89377b97100ba8fd54ccd
NeverForgetone <820211598@qq.com>
2023-06-08 18:25:41 +0800

    camera ota  程序示例项目

---
+++ OTA/source/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    pthread_t tid[10];
+    int i;//to iterate over the array of thread id's.
+    printf("main thread runing ...");
+    return 0;
+}
